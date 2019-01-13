05a868108da6234bfc8889f4da5ec8a317e2620a
nirpaw <32441694+nirpaw@users.noreply.github.com>
2019-01-13 02:03:15 +0200

    Scrolling activity added... on click listener on recyucler view

--- app/src/main/java/com/example/nir30/newsly2/MainActivity.java
+++ app/src/main/java/com/example/nir30/newsly2/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.nir30.newsly2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<NewsArticle> newsArticles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.news_recycler);
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(this, recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        Intent intent = new Intent(MainActivity.this, ShowArticleActivity.class);
+                        startActivity(intent);
+                    }
 
+                    @Override public void onLongItemClick(View view, int position) {
+                        Intent sendIntent = new Intent();
+                        sendIntent.setAction(Intent.ACTION_SEND);
+                        sendIntent.putExtra(Intent.EXTRA_TEXT, newsArticles.get(position).getTitle() +": " +newsArticles.get(position).getUrl() );
+                        sendIntent.setType("text/plain");
+                        startActivity(sendIntent);
+                    }
+                })
+        );
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(this,1));
 
-        List<NewsArticle> newsArticles = new ArrayList<>();
         newsArticles.add(new NewsArticle("test"));
         newsArticles.add(new NewsArticle("test"));
-
         newsArticles.add(new NewsArticle("test"));
         newsArticles.add(new NewsArticle("test"));
         newsArticles.add(new NewsArticle("test"));
---
+++ app/src/main/java/com/example/nir30/newsly2/ShowArticleActivity.java
@@ -0,0 +1,33 @@
+package com.example.nir30.newsly2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ShowArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_article);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yourwebsite.com"));
+                startActivity(intent);
+
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
